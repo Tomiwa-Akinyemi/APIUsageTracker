0013446b62ff456d3cf9c6b9933380b4febec1fe
Tomiwa Akinyemi <takinyemi@appzonegroup.com>
2021-10-15 16:48:38 +0100

    Add project files.

---
+++ APIUsage.Core/Fee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace APIUsage.Core
+{
+    public class Fee
+    {
+        public double BandFrom { get; set; }
+        public double BandTo { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
